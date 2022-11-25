9db0143e049883d6dc7df210a00ada471a98a40b
HARIHARAN2262004 <118712251+HARIHARAN2262004@users.noreply.github.com>
2022-11-25 16:32:34 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import New from './New';
+
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <New/>
+    </div>
+  );
+}
+
+export default App;
